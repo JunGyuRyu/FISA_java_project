8afc9a4ccf75fcd7c82ed763d4faeb12b6f5a30f
JunGyuRyu <qurrpdnjs1@naver.com>
2024-01-12 13:58:24 +0900

    update main

--- todoApp/src/todoApp/mainTodo.java
+++ todoApp/src/todoApp/mainTodo.java
@@ -1,18 +1,41 @@
 package todoApp;
 
+import java.util.ArrayList;
+
+
 public class mainTodo {
 	public static void main(String[] argrs) {
 		
-		Todo todo1 = new Todo();
-		todo1.setIsDone(false);
-		todo1.setPriority(1);
-		todo1.setTitle("할일1");
-		System.out.println(todo1.getIsDone());
-		System.out.println(todo1.getPriority());
-		System.out.println(todo1.getTitle());
-		System.out.println(todo1);
+		User user01 = new User();
+		user01.setAge(10);
+		user01.setGender("m");
+		user01.setName("user01");
+		user01.addTodo("할일1", 1);
+		System.out.println(user01.getAge());
+		System.out.println(user01.getGender());
+		System.out.println(user01.getName());
+		
+		User user02 = new User("user02", 12, "f");
+		System.out.println(user02.getAge());
+		System.out.println(user02.getGender());
+		System.out.println(user02.getName());
+		
+		user02.addTodo("todo1", 1);
+		System.out.println(user02.remainList());
+		System.out.println(user02.doneList());
 		
 		
+		user02.addTodo("todo2", 1);
+		System.out.println(user02.remainList());
+		System.out.println(user02.doneList());
+		
+		user02.addTodo("todo3", 1);
+		System.out.println(user02.remainList());
+		System.out.println(user02.doneList());
+		
+		user02.deleteTodo("todo2");
+		System.out.println(user02.remainList());
+		System.out.println(user02.doneList());
 		
 		
 	}
