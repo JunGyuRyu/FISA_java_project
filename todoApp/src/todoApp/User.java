372fab8ffeb8455e4708fcc38d58971f5eb214bb
GayoungKim <gyk7@naver.com>
2024-01-12 12:44:31 +0900

    update thisuser

--- todoApp/src/todoApp/User.java
+++ todoApp/src/todoApp/User.java
@@ -9,6 +9,10 @@ public class User {
 	private ArrayList<Todo> todoList;
 	private ArrayList<Todo> doneList;
 	
+	User(){
+		
+	}
+	
 	public String getName() {
 		return name;
 	}
@@ -38,32 +42,32 @@ public class User {
 		Todo t1 = new Todo();
 		t1.setTitle(title);
 		t1.setPriority(priority);
-		todoList.add(t1);
+		this.todoList.add(t1);
 	}
 	
 	void deleteTodo(String title) {
-		for (int i = 0 ; i < todoList.size() ; i ++) {
-			if (title == todoList.get(i).getTitle()) {
-				doneList.add(todoList.get(i));
-				todoList.remove(i);
+		for (int i = 0 ; i < this.todoList.size() ; i ++) {
+			if (title == this.todoList.get(i).getTitle()) {
+				this.doneList.add(this.todoList.get(i));
+				this.todoList.remove(i);
 				
 			}
 		}
 		
 	}
 	  
-//	ArrayList<Todo> doneList() {
-//		
-//	}
-//	
-//	ArrayList<Todo> remainList() {
-//		
-//	}
+	ArrayList<Todo> doneList() {
+		return this.doneList;
+	}
+	
+	ArrayList<Todo> remainList() {
+		return this.todoList;
+	}
 	
 	void introduce() {
 		System.out.println("-----개인정보-----");
-		System.out.println("이름 : " + name);
-		System.out.println("나이 : " + age);
-		System.out.println("성별 : " + gender);
+		System.out.println("이름 : " + this.name);
+		System.out.println("나이 : " + this.age);
+		System.out.println("성별 : " + this.gender);
 	}
 }
